fa1dd2c7f21c4d53f8ea3dccf1a35212e2d4f8a0
manoj <manoj.t@transneuron.com>
2018-11-26 16:49:31 +0530

    added local toml

---
+++ lib/hugo.sh
@@ -0,0 +1,7 @@
+
+#!/bin/sh
+# Install a Hugo, https://gohugo.io/
+#
+# You can either add this here, or configure them on the environment tab of your
+# project settings.
+hugo serve --disableFastRender --config local.toml
